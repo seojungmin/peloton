98dd8c09da1eb8f727ad278dc2a1c3e2630c75df
Jungmin Seo <seojungmin@gmail.com>
2017-08-01 18:06:49 -0400

    Remove all the redundant code in DeletePlan

--- src/planner/update_plan.cpp
+++ src/planner/update_plan.cpp
@@ -11,11 +11,9 @@
 //===----------------------------------------------------------------------===//
 
 #include "planner/update_plan.h"
-
 #include "planner/project_info.h"
-#include "type/types.h"
-
 #include "storage/data_table.h"
+#include "type/types.h"
 
 namespace peloton {
 namespace planner {
@@ -37,7 +35,6 @@ UpdatePlan::UpdatePlan(storage::DataTable *table,
     }
   }
 }
-
 
 void UpdatePlan::SetParameterValues(std::vector<type::Value> *values) {
   LOG_TRACE("Setting parameter values in Update");
